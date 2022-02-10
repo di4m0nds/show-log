ea8783bd72630a9c934203d0175739e5960f2603
di4m0nds <ws.2000.sja@gmail.com>
2022-02-09 21:28:56 -0300

    Exports functions

---
+++ index.js
@@ -0,0 +1,7 @@
+const simpleShow = require('./simpleShow.js')
+const showSpecificLine = require('./showSpecificLine.js')
+
+module.exports = {
+  showLog: simpleShow,
+  showThisLine: showSpecificLine
+}
